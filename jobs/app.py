460f1e180ba256686c6c8114fd1313ee11c0647a
Owen Utley <90651143+owenutley@users.noreply.github.com>
2021-11-22 11:44:06 -0700

    adjusted open_connection function

--- jobs/app.py
+++ jobs/app.py
@@ -6,7 +6,7 @@ PATH = 'db/jobs.sqlite'
 app = Flask('__name__')
 
 def open_connnection():
-    connection = getattr(g, 'connection', None)
+    connection = getattr(g, '_connection', None)
     if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
