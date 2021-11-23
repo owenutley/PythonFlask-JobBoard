949dab1fb369f748a428e6511b00da4e8a8b3810
Owen Utley <90651143+owenutley@users.noreply.github.com>
2021-11-22 17:07:37 -0700

    fixed?

--- jobs/app.py
+++ jobs/app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, g
 
 PATH = 'db/jobs.sqlite'
 
-app = Flask('__name__')
+app = Flask(__name__)
 
 def open_connection():
     connection = getattr(g, '_connection', None)
@@ -33,19 +33,16 @@ def close_connection(exception):
 @app.route('/jobs')
 def jobs():
     jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
-    
     return render_template('index.html', jobs=jobs)
 
 @app.route('/job/<job_id>')
 def job(job_id):
     job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?', [job_id], single=True)
-    
     return render_template('job.html', job=job)
 
 @app.route('/employer/<employer_id>')
 def employer(employer_id):
-    employer = execute_sql('SELECT * FROM employer WHERE id=?',
-    [employer_id], simgle=True)
+    employer = execute_sql('SELECT * FROM employer WHERE id=?', [employer_id], single=True)
     jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary FROM job JOIN employer ON employer.id = job.employer_id WHERE employer.id = ?', [employer_id])
     reviews = execute_sql('SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?', [employer_id])
     return render_template('employer.html', employer=employer, jobs=jobs, reviews=reviews)
